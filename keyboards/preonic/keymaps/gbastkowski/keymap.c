883eb23dc7d3d9aa1b1f5a3c51133441af80a999
Gunnar Bastkowski <gunnar@bastkowski.name>
2024-05-07 00:37:43 +0200

    Change keybard layouts

--- keyboards/preonic/keymaps/gbastkowski/keymap.c
+++ keyboards/preonic/keymaps/gbastkowski/keymap.c
@@ -1,10 +1,23 @@
 #include QMK_KEYBOARD_H
+#include "muse.h"
 
+// Each layer gets a name for readability, which is then used in the keymap matrix below.
 #define L_QWERTSPLITLY 0
 #define L_FN 1
 
+// Defines for Mac specific key accelerators used in map. Some of these are very awkward to type.
+// These are the default bindings for Mac, some of which may be turned off or changed, check your
+// settings in System Preferences
+#define LS_TAB LSFT(KC_TAB) // Shift-TAB
+#define MF_MENU LCTL(KC_F2) // Move focus to menu bar default keybinding in MacOS
+#define MF_DOCK LCTL(KC_F3) // Move focus to dock
+#define MF_TOOL LCTL(KC_F5) // Move focus to window toolbar
+#define MF_STAT LCTL(KC_F8) // Move focus to status bar side of menu bar
+#define MF_DRWR LALT(LGUI(KC_QUOTE)) // Move focus to window drawer
+#define MF_LPAD LALT(LGUI(KC_Q)) // Show Launchpad
 #define MF_NOTI LALT(LGUI(LCTL(KC_B))) // Show Notification Center
 #define MF_DASH LALT(LGUI(LCTL(KC_C))) // Show Dashboard
+#define MF_HELP LSFT(LGUI(KC_SLASH)) // Position in menu Help search box
 
 // Momentary/Tap Combos used by this layout
 #define LT_FNESC LT(L_FN, KC_ESC) // ESC when tapped, Fn layer when held
@@ -22,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* QwertSplitly
  * ,------------------------------------------------------------------------------------------------------------------------.
- * |    1    |    2    |    3    |    4    |    5    |    =    ||    -    |    6    |    7    |    8    |    9    |    0    |
+ * |    1    |    2    |    3    |    4    |    5    |    -    ||    =    |    6    |    7    |    8    |    9    |    0    |
  * |---------+---------+---------+---------+---------+---------++---------+---------+---------+---------+---------+---------|
  * |    Q    |    W    |    E    |    R    |    T    |    \    ||    '    |    Y    |    U    |    I    |    O    |    P    |
  * |---------+---------+---------+---------+---------+---------++---------+---------+---------+---------+---------+---------|
@@ -34,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `------------------------------------------------------------------------------------------------------------------------'
  */
 [L_QWERTSPLITLY] = LAYOUT_preonic_grid( \
-    KC_1     ,KC_2     ,KC_3     ,KC_4     ,KC_5     ,KC_EQL    ,KC_MINUS ,KC_6     ,KC_7     ,KC_8     ,KC_9     ,KC_0     ,\
+    KC_1     ,KC_2     ,KC_3     ,KC_4     ,KC_5     ,KC_MINUS  ,KC_EQL   ,KC_6     ,KC_7     ,KC_8     ,KC_9     ,KC_0     ,\
     KC_Q     ,KC_W     ,KC_E     ,KC_R     ,KC_T     ,KC_BSLS   ,KC_QUOTE ,KC_Y     ,KC_U     ,KC_I     ,KC_O     ,KC_P     ,\
     KC_A     ,KC_S     ,KC_D     ,KC_F     ,KC_G     ,KC_LBRC   ,KC_RBRC  ,KC_H     ,KC_J     ,KC_K     ,KC_L     ,KC_SCLN  ,\
     KC_Z     ,KC_X     ,KC_C     ,KC_V     ,KC_B     ,KC_TAB    ,MK_RALT  ,KC_N     ,KC_M     ,KC_COMM  ,KC_DOT   ,KC_SLSH  ,\
@@ -59,7 +72,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______  ,_______  ,MK_LPAR  ,MK_RPAR  ,_______  ,MK_TILD   ,KC_GRV   ,_______  ,_______  ,_______  ,_______  ,_______  ,\
     _______  ,_______  ,KC_LBRC  ,KC_RBRC  ,_______  ,_______   ,_______  ,KC_LEFT  ,KC_DOWN  ,KC_UP    ,KC_RGHT  ,_______  ,\
     _______  ,_______  ,MK_LBRA  ,MK_RBRA  ,_______  ,_______   ,TG_FN    ,_______  ,_______  ,_______  ,_______  ,_______  ,\
-    _______  ,_______  ,_______  ,_______  ,_______  ,_______   ,_______  ,KC_DEL   ,_______  ,_______  ,_______  ,_______ \
+    KC_CAPS  ,_______  ,_______  ,_______  ,_______  ,_______   ,_______  ,KC_DEL   ,_______  ,_______  ,_______  ,_______ \
 )
 
 };
@@ -79,31 +92,31 @@ float music_scale[][2]     = SONG(MUSIC_SCALE_SOUND);
 
 void matrix_init_user(void) {
     #ifdef AUDIO_ENABLE
-        startup_user();
+        /* startup_user(); */
     #endif
 }
 
 #ifdef AUDIO_ENABLE
 
-void startup_user()
-{
-    PLAY_SONG(tone_startup);
-}
+/* void startup_user() */
+/* { */
+/*     PLAY_SONG(tone_startup); */
+/* } */
 
-void shutdown_user()
-{
-    PLAY_SONG(tone_goodbye);
-    stop_all_notes();
-}
+/* void shutdown_user() */
+/* { */
+/*     PLAY_SONG(tone_goodbye); */
+/*     stop_all_notes(); */
+/* } */
 
-void music_on_user(void)
-{
-    music_scale_user();
-}
+/* void music_on_user(void) */
+/* { */
+/*     music_scale_user(); */
+/* } */
 
-void music_scale_user(void)
-{
-    PLAY_SONG(music_scale);
-}
+/* void music_scale_user(void) */
+/* { */
+/*     PLAY_SONG(music_scale); */
+/* } */
 
 #endif
