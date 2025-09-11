789b8e0593f6685fd753bd392621dca62b9917bc
Gunnar Bastkowski <gunnar@bastkowski.name>
2025-09-11 20:43:39 +0200

    Add steno layer to ID75 keymap

--- keyboards/idobao/id75/keymaps/gbastkowski/keymap.c
+++ keyboards/idobao/id75/keymaps/gbastkowski/keymap.c
@@ -19,6 +19,7 @@
 #define _QW 0
 #define _FN 1
 #define _KB 2
+#define _ST 3
 
 // Momentary/Tap Combos used by this layout
 #define LT_FNESC LT(_FN, KC_ESC) // ESC when tapped, Fn layer when held
@@ -30,6 +31,8 @@
 #define MK_RALT OSM(MOD_RALT)
 #define TG_FN   TG(_FN)
 #define TG_KB   TG(_KB)
+#define TG_ST   TG(_ST)
+#define TO_QW   TO(_QW)
 #define TO_FN   TO(_FN)
 #define TO_KB   TO(_KB)
 
@@ -49,21 +52,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, MK_LPAR, MK_RPAR, _______, MK_TILD,     KC_GRV,  KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______,     _______, _______, _______,
         _______, _______, KC_LBRC, KC_RBRC, _______, _______,     _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______,     _______, _______, TG_KB,
         _______, _______, MK_LBRA, MK_RBRA, _______, _______,     _______, _______, _______, _______, _______, _______,     _______, _______, _______,
-        KC_CAPS, _______, _______, _______, _______, _______,     _______, KC_DEL,  _______, _______, _______, _______,     _______, _______, QK_BOOT
+        KC_CAPS, _______, _______, _______, _______, _______,     _______, KC_DEL,  _______, _______, _______, _______,     _______, TG_ST,   QK_BOOT
     ),
 
     [_KB] = LAYOUT_ortho_5x15(/* FUNCTION */
         _______, _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______, _______,     _______, _______, _______,
-        _______, _______, _______, _______, RGB_HUD, RGB_HUI,     _______, _______, _______, _______, _______, _______,     _______, _______, _______,
-        _______, _______, _______, _______, RGB_SAD, RGB_SAI,     _______, _______, _______, _______, _______, _______,     _______, _______, TG_KB,
-        _______, _______, _______, _______, RGB_VAD, RGB_VAI,     _______, _______, _______, _______, _______, _______,     _______, _______, _______,
-        _______, _______, _______, RGB_TOG, RGB_RMOD,RGB_MOD,     _______, _______, _______, _______, _______, _______,     _______, _______, _______
+        _______, _______, _______, _______, QK_RGB_MATRIX_HUE_DOWN, QK_RGB_MATRIX_HUE_UP,     _______, _______, _______, _______, _______, _______,     _______, _______, _______,
+        _______, _______, _______, _______, QK_RGB_MATRIX_SATURATION_DOWN, QK_RGB_MATRIX_SATURATION_UP,     _______, _______, _______, _______, _______, _______,     _______, _______, TG_KB,
+        _______, _______, _______, _______, QK_RGB_MATRIX_VALUE_DOWN, QK_RGB_MATRIX_VALUE_UP,     _______, _______, _______, _______, _______, _______,     _______, _______, _______,
+        _______, _______, _______, QK_RGB_MATRIX_TOGGLE, QK_RGB_MATRIX_MODE_PREVIOUS, QK_RGB_MATRIX_MODE_NEXT,     _______, _______, _______, _______, _______, _______,     _______, _______, _______
+    ),
+
+    [_ST] = LAYOUT_ortho_5x15(/* STENO (GeminiPR layout) */
+        _______, STN_N1,  STN_N2,  STN_N3,  STN_N4,  STN_N5,     STN_N6,  STN_N7,  STN_N8,  STN_N9,  STN_NA,  STN_NB,     STN_NC,  _______, _______,
+        _______, STN_S1,  STN_TL,  STN_PL,  STN_HL,  STN_ST1,    STN_ST3, STN_FR,  STN_PR,  STN_LR,  STN_TR,  STN_DR,     _______, _______, _______,
+        _______, STN_S2,  STN_KL,  STN_WL,  STN_RL,  STN_ST2,    STN_ST4, STN_RR,  STN_BR,  STN_GR,  STN_SR,  STN_ZR,     _______, TG_ST,   _______,
+        _______, _______, _______, _______, _______, STN_A,       STN_O,   STN_E,   STN_U,   _______, _______, _______,     _______, _______, _______,
+        _______, _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______, _______,     _______, _______, TO_QW
     ),
 
 };
 
 
-/* void set_hsv_at(HSV hsv, uint8_t index); */
+void set_hsv_at(HSV hsv, uint8_t index);
 
 /* bool process_record_user(uint16_t keycode, keyrecord_t *record) { */
 /*     return true; */
@@ -75,11 +86,12 @@ bool rgb_matrix_indicators_user(void) {
         HSV hsv_inv_hue = {hsv.h + 128, hsv.s, hsv.v};
         set_hsv_at(hsv_inv_hue, 30);
     }
-    indicate which higher layer is currently active on which key
+    // indicate which higher layer is currently active on which key
     HSV hsv1 = {hsv.h - 64, hsv.s, hsv.v};
     HSV hsv2 = {hsv.h + 64, hsv.s, hsv.v};
-    except when we are in any animated LED mode
-    if (rgb_matrix_config.mode < RGB_MATRIX_BREATHING || rgb_matrix_config.mode > RGB_MATRIX_JELLYBEAN_RAINDROPS) {
+    // except when we are in any animated LED mode
+    // Only apply indicators when not in a standard animation mode
+    if (rgb_matrix_config.mode < 1 || rgb_matrix_config.mode >= RGB_MATRIX_EFFECT_MAX) {
     for (uint8_t row = 0; row < MATRIX_ROWS; row++) {
         for (uint8_t col = 0; col < MATRIX_COLS; col++) {
             keypos_t pos = {col, row};
@@ -103,13 +115,13 @@ bool rgb_matrix_indicators_user(void) {
         }
     }
     }
-    sticky layer indicators regardless of RGB mode. Hardcoded for lack of better idea.
+    // sticky layer indicators regardless of RGB mode. Hardcoded for lack of better idea.
     set_hsv_at(hsv1, 63);
     set_hsv_at(hsv2, 57);
     return false;
 }
 
-/* void set_hsv_at(HSV hsv, uint8_t index) { */
-/*     RGB rgb = hsv_to_rgb(hsv); */
-/*     rgb_matrix_set_color(index, rgb.r, rgb.g, rgb.b); */
-/* } */
+void set_hsv_at(HSV hsv, uint8_t index) {
+    RGB rgb = hsv_to_rgb(hsv);
+    rgb_matrix_set_color(index, rgb.r, rgb.g, rgb.b);
+}
---
+++ keyboards/idobao/id75/keymaps/gbastkowski/rules.mk
@@ -0,0 +1,5 @@
+STENO_ENABLE = yes
+# Select protocol; 'all' enables protocol-switching keycodes, but uses more space.
+# Options: geminipr | txbolt | all
+STENO_PROTOCOL = geminipr
+
